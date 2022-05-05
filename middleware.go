ca7e58f8ba2a8de13211e27d1b8094299345ad81
Michael Teuscher <michael.teuscher@pm.me>
2022-05-06 00:03:31 +0200

    Add Log to Middleware

--- middleware.go
+++ middleware.go
@@ -1,7 +1,13 @@
 package plausible
 
+import "github.com/go-mojito/mojito"
+
 // PlausibleMiddleware provides automatic visit tracking for all routes
-func PlausibleMiddleware(ctx PlausibleContext, next func() error) error {
-	go ctx.PageView()
+func PlausibleMiddleware(ctx PlausibleContext, logger mojito.Logger, next func() error) error {
+	go func(ctx PlausibleContext) {
+		if err := ctx.PageView(); err != nil {
+			logger.Error(err)
+		}
+	}(ctx)
 	return next()
 }
